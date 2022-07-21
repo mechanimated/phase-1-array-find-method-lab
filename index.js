96e5108296fcf303867236d5294155a207b3a4c2
Liz Boika <mdaicdxl@gmail.com>
2022-07-21 15:02:27 -0400

    commit tree a83629e71a0837f3d0db548d796961c2a640afd1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function finder(item) {
+    return item.result === 'W';
+}
+
+const superbowlWin = function(array) {
+    let resultWL = array.find(finder)
+        if(resultWL) {
+            return resultWL.year;
+        }
+      
+}
+
+
+superbowlWin(sadReality);
